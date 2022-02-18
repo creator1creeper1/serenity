cfd13fe2c0f99159f22b89d24cea283a9929a4f9
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2022-02-18 14:56:21 +0100

    Lagom: Add LibSyntax port
    Also make use of that to enable some previously excluded
    componentes in the LibGUI port.

--- Meta/Lagom/Libraries/LibGUI/Clipboard.h
+++ Meta/Lagom/Libraries/LibGUI/Clipboard.h
@@ -19,7 +19,32 @@ class ClipboardServerConnection;
 
 class Clipboard {
 public:
+    class ClipboardClient {
+    public:
+        ClipboardClient() { }
+        virtual ~ClipboardClient() { }
+
+        virtual void clipboard_content_did_change(String const& mime_type) = 0;
+    };
+
+    struct DataAndType {
+        ByteBuffer data;
+        String mime_type;
+        HashMap<String, String> metadata;
+
+        RefPtr<Gfx::Bitmap> as_bitmap() const;
+    };
+
     static void initialize(Badge<Application>) { }
+    static Clipboard& the();
+
+    DataAndType fetch_data_and_type() const { return {}; }
+    String fetch_mime_type() const { return ""; }
+
+    void set_data(ReadonlyBytes, String const&, HashMap<String, String> const&) { }
+    void set_plain_text(String const&) { }
+    void set_bitmap(Gfx::Bitmap const&) { }
+    void clear() { }
 
 private:
     Clipboard() = default;
