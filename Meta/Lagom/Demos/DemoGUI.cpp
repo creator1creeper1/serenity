c5f4401fe343247f6c37367d0c92c12a3f58ba31
Ali Mohammad Pur <ali.mpfard@gmail.com>
2022-02-18 17:48:26 +0330

    stuff

--- Meta/Lagom/Demos/DemoGUI.cpp
+++ Meta/Lagom/Demos/DemoGUI.cpp
@@ -4,21 +4,32 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <LibCore/System.h>
-#include <LibGUI/AboutDialog.h>
 #include <LibGUI/Application.h>
 #include <LibGUI/BoxLayout.h>
 #include <LibGUI/Button.h>
-#include <LibGUI/Label.h>
-#include <LibGUI/MessageBox.h>
 #include <LibGUI/Window.h>
 #include <LibGfx/FontDatabase.h>
 #include <LibGfx/Palette.h>
 #include <LibGfx/SystemTheme.h>
 #include <LibMain/Main.h>
+#include <LibWasm/Types.h>
 
-ErrorOr<int> serenity_main(Main::Arguments arguments)
+RefPtr<GUI::Application> app;
+
+extern "C" {
+
+__attribute__((constructor)) void ctor()
 {
+    char* argv[] {
+        strdup("magic"),
+        nullptr,
+    };
+    Main::Arguments arguments {
+        1,
+        argv,
+        {},
+    };
+
     // Set default font path.
     // TODO: figure out a better way to do this :^)
     Gfx::FontDatabase::set_default_fonts_lookup_path("../../Base/res/fonts");
@@ -27,34 +38,21 @@ ErrorOr<int> serenity_main(Main::Arguments arguments)
     // Ditto but for the theme and palette
     Gfx::set_system_theme(Gfx::load_system_theme("../../Base/res/themes/Default.ini"));
 
-    auto app = TRY(GUI::Application::try_create(arguments));
+    app = MUST(GUI::Application::try_create(arguments));
     app->set_system_palette(Gfx::current_system_theme_buffer());
 
-    auto window = TRY(GUI::Window::try_create());
+    auto window = MUST(GUI::Window::try_create());
     window->set_title("Hello LibGUI World");
     window->resize(600, 400);
 
-    auto widget = TRY(window->try_set_main_widget<GUI::Widget>());
-    widget->set_fill_with_background_color(true);
-
-    auto layout = TRY(widget->try_set_layout<GUI::VerticalBoxLayout>());
-    layout->set_margins(16);
-
-    auto label = TRY(widget->try_add<GUI::Label>("Hello World :^)"));
-
-    auto first_button = TRY(widget->try_add<GUI::Button>("A very cool button"));
-    first_button->on_click = [&](auto) {
-        GUI::MessageBox::show(window, "Hello friends!", ":^)");
-    };
-
-    auto second_button = TRY(widget->try_add<GUI::Button>("This button is disabled :^("));
-    second_button->set_enabled(false);
-
-    auto third_button = TRY(widget->try_add<GUI::Button>("About"));
-    third_button->on_click = [&](auto) {
-        GUI::AboutDialog::show("SerenityOS", nullptr, nullptr, nullptr, Core::Version::read_long_version_string());
-    };
-
+    auto widget = MUST(window->try_set_main_widget<GUI::Widget>());
     window->show();
-    return app->exec();
+}
+
+__attribute__((destructor)) void dtor()
+{
+    app = nullptr;
+}
+
+Wasm::Value
 }
